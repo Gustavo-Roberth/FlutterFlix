d245dc7e996dbdca20608fbba33ebd6371d437ca
Gustavo Roberth <gustavoroberthc@gmail.com>
2021-03-14 16:16:33 -0300

    Criação de cards de filmes da home_page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_flix/controllers/movie_controller.dart';
+import 'package:flutter_flix/widgets/movie_card.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -6,27 +8,76 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final _controller = MovieController();
+  final _scrollController = ScrollController();
+  int lastPage = 1;
+
   @override
   void initState() {
     super.initState();
+    _initScrollListener();
+    _initialize();
+  }
+
+  _initScrollListener() {
+    _scrollController.addListener(() async {
+      if (_scrollController.offset >=
+          _scrollController.position.maxScrollExtent) {
+        if (_controller.currentPage == lastPage) {
+          lastPage++;
+          await _controller.fetchAllMovies(page: lastPage);
+          setState(() {});
+        }
+      }
+    });
+  }
+
+  _initialize() async {
+    setState(() {
+      _controller.loading = true;
+    });
+
+    await _controller.fetchAllMovies(page: lastPage);
+
+    setState(() {
+      _controller.loading = false;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('FlutterFlix'),
-        actions: [
-          IconButton(
-            icon: Icon(Icons.favorite),
-            onPressed: () {},
+        appBar: AppBar(
+          title: Text('FlutterFlix'),
+          actions: [
+            IconButton(
+              icon: Icon(Icons.favorite),
+              onPressed: () {},
+            ),
+            IconButton(
+              icon: Icon(Icons.search),
+              onPressed: () {},
+            ),
+          ],
+        ),
+        body: GridView.builder(
+          shrinkWrap: true,
+          controller: _scrollController,
+          padding: const EdgeInsets.all(2.0),
+          itemCount: _controller.moviesCount,
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            mainAxisSpacing: 5,
+            crossAxisSpacing: 5,
+            childAspectRatio: 0.65,
           ),
-          IconButton(
-            icon: Icon(Icons.search),
-            onPressed: () {},
-          ),
-        ],
-      ),
-    );
+          itemBuilder: (context, index) {
+            final movie = _controller.movies[index];
+            return MovieCard(
+              posterPath: movie.posterPath,
+              onTap: () {},
+            );
+          },
+        ));
   }
 }
