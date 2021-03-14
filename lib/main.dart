a7fd2dee648aa19dc1714fc6c38828182c374493
Gustavo Roberth <gustavoroberthc@gmail.com>
2021-03-14 18:53:45 -0300

    Adicionado ícone de launcher e SplashScreen de inicialização

--- lib/main.dart
+++ lib/main.dart
@@ -2,18 +2,63 @@ import 'package:flutter/material.dart';
 import 'package:flutter_flix/core/constant.dart';
 import 'package:flutter_flix/core/theme_app.dart';
 import 'package:flutter_flix/pages/home_page.dart';
+import 'package:splashscreen/splashscreen.dart';
 
 void main() {
-  runApp(MyApp());
+  // main do app
+  runApp(FlutterFlix());
 }
 
-class MyApp extends StatelessWidget {
+// classe Material principal
+class FlutterFlix extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: kAppName,
       theme: kThemeApp,
-      home: HomePage(),
+      home: IntroPage(),
     );
   }
+}
+
+// classe que realiza a chamada para a primeira tela
+// Tela de inicialização
+class IntroPage extends StatefulWidget {
+  @override
+  IntroPageState createState() => IntroPageState();
+}
+
+class IntroPageState extends State<IntroPage> {
+  @override
+  Widget build(BuildContext context) {
+    return _introScreen();
+  }
+}
+
+// Predefinições para a Tela de inicialização
+Widget _introScreen() {
+  return Stack(
+    children: <Widget>[
+      SplashScreen(
+        seconds: 5,
+        gradientBackground: LinearGradient(
+          begin: Alignment.topRight,
+          end: Alignment.bottomLeft,
+          colors: [Color(0xffFFD452), Color(0xffFFD452)],
+        ),
+        navigateAfterSeconds: HomePage(),
+        loaderColor: Colors.transparent,
+      ),
+      Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/icons/icon.png"),
+            scale: 4.0,
+            fit: BoxFit.none,
+          ),
+        ),
+      ),
+    ],
+  );
 }
